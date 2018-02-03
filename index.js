0459531f0d2de7da34e5c9a0f8991e3952da0c13
lnoellaho <lnoellaho@gmail.com>
2018-02-02 21:34:24 -0800

    alexa tech talk game

--- index.js
+++ index.js
@@ -1,33 +1,70 @@
 "use strict";
 
-// Include the Alexa SDK
 var Alexa = require("alexa-sdk");
 
-// The handlers object tells Alexa how to handle various actions
+var randomNum = 0
+var turns = 0
+
 var handlers = {
-  "HelloIntent": function () {
-		//Create speech output. This is what Alexa will speak back when the user says "Ask code academy to say hello"
-      this.response.speak("Hello, Codecademy");
-      this.emit(":responseReady");
-   },
-  //Our skill will receive a LaunchRequest when the user invokes the skill with the invocation name, but does not provide any command mapping to an intent. For example, "Open code academy"
-  "LaunchRequest": function () {
-    this.response.speak("Welcome to Codecademy");
-    this.emit(":responseReady");
-   }
+  'LaunchRequest': function() {
+    randomNum = Math.floor(Math.random()*11)
+
+    this.response.speak("Welcome to the High Low Game. I am thinking of a number from zero to ten. If you guess the number correctly in five tries, you win. Take a guess.").listen("Guess a number from zero to ten.");
+    this.emit(':responseReady');
+  },
+
+  // Number guess function
+  'NumberIntent': function () {
+    var guess = this.event.request.intent.slots.number.value;
+
+    if(turns < 4) {
+        if(guess > 10) {
+            this.response.speak("Please guess a number from zero to ten.").listen("Please guess a number from zero to ten.")
+        } else if(guess == randomNum) {
+            this.response.speak("Yay! " + guess + " is the number I was thinking of. You win! Thank you for playing the High Low Game, let\'s play again soon.");
+        } else if(guess > randomNum) {
+            this.response.speak(guess + " is too high. Try again.").listen("Guess again.");
+        } else if(guess < randomNum) {
+            this.response.speak(guess + " is too low. Try again.").listen("Guess again.");
+        }
+    } else {
+        if(guess > 10) {
+            this.response.speak("Please guess a number from zero to ten.").listen("Please guess a number from zero to ten.")
+        } else if(guess == randomNum) {
+            this.response.speak("Yay! " + guess + " is the number I was thinking of. You win! Thank you for playing the High Low Game, let\'s play again soon.");
+        } else if(guess > randomNum) {
+            this.response.speak(guess + " is too high. You ran out of turns. You lose. Try again next time.");
+        } else if(guess < randomNum) {
+            this.response.speak(guess + " is too low. You ran out of turns. You lose. Try again next time.");
+        }
+    }
+    turns++
+    this.emit(':responseReady');
+  },
+
+  // Stop
+  'AMAZON.StopIntent': function() {
+      this.response.speak('Ok, let\'s play again soon.');
+      this.emit(':responseReady');
+  },
+
+  'AMAZON.HelpIntent': function() {
+      this.response.speak('I am thinking of a number from zero to ten. Guess the number correctly, and you win. ').listen("Guess a number from zero to ten.");
+      this.emit(':responseReady');
+  },
+
+  // Cancel
+  'AMAZON.CancelIntent': function() {
+      this.response.speak('Ok, let\'s play again soon.');
+      this.emit(':responseReady');
+  }
 
 };
 
 
-// This is the function that AWS Lambda calls every time Alexa uses your skill.
-exports.handler = function(event, context, callback) {
 
-  // Set up the Alexa object
-	var alexa = Alexa.handler(event, context);
-
-  // Register Handlers
-  alexa.registerHandlers(handlers);
-
-  // Start our Alexa code
-  alexa.execute();
+exports.handler = function(event, context, callback){
+    var alexa = Alexa.handler(event, context);
+    alexa.registerHandlers(handlers);
+    alexa.execute();
 };
